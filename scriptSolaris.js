07f5039dd9234a59870eee80d7b951bd08cb83db
Sara <sara_lunden1@hotmail.com>
2024-12-02 14:42:05 +0100

    api updates on js

--- scriptSolaris.js
+++ scriptSolaris.js
@@ -1,2 +1,35 @@
 const body = document.getElementById("body");
-body.style.background = "linear-gradient(90deg rgba(9, 4, 28, 1), rgba(59, 54, 113, 1))";
+
+async function fetchData() {
+  //Making async function with the data we're requesting to fetch.
+  const urlApi =
+    "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies";
+  const urlApiKey =
+    "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys";
+
+  try {
+    const response = await fetch(urlApiKey, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (!response.ok) {
+      throw new console.error("Can not fetch"); //If the response isn't okey it will print a error msg.
+    }
+    const keydata = await response.json();
+    const keyApi = keydata.key;
+
+    const response2 = await fetch(urlApi, {
+      method: "GET",
+      headers: { "x-zocom": `${keyApi}` },
+    });
+    
+    const data = await response2.json();
+    console.log(data);
+  }
+   catch (error) {
+    console.error(error);
+  }
+}
+
+fetchData();
